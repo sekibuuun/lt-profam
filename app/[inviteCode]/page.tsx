c2bc83ea3d2b32e0b9545e6c99e571bf1910d95e
sekibuuun <akaunto.no.001@gmail.com>
2025-02-24 22:47:46 +0900

    fix: レビュー修正

--- app/[inviteCode]/page.tsx
+++ app/[inviteCode]/page.tsx
@@ -1,12 +1,11 @@
 import { notFound } from "next/navigation";
-import { getInviteId, isValidInvite, getInvite } from "@/lib/db";
+import { isValidInvite, getFiles } from "@/lib/db";
 import ClientInvitePage from "@/components/ClientInvitePage";
 
 export default async function InvitePage({ params }: { params: { inviteCode: string } }) {
   if (!(await isValidInvite(params.inviteCode))) {
     notFound();
   }
-  const id = await getInviteId(params.inviteCode);
-  const invite = await getInvite(id);
-  return <ClientInvitePage initialFiles={invite.files} inviteId={id}/>;
+  const files = await getFiles(params.inviteCode);
+  return <ClientInvitePage initialFiles={files}/>;
 }
