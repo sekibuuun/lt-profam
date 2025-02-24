0cd6d44c61bddc16cc513d8d60a5fdb8ea401463
sekibuuun <akaunto.no.001@gmail.com>
2025-02-24 17:53:13 +0900

    fix: スキーマ変更

--- db/schema.ts
+++ db/schema.ts
@@ -1,15 +1,14 @@
-import { integer, text, boolean, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { text, timestamp, pgTable, serial, integer } from "drizzle-orm/pg-core";
 
 export const invites = pgTable("invites", {
-  id: integer("id").primaryKey(),
+  id: serial("id").primaryKey(),
   code: text("code").notNull(),
-  used: boolean("used").default(false).notNull(),
   createdAt: timestamp("created_at", { mode: "string" }).defaultNow().notNull(),
 });
 
 export const files = pgTable("files", {
-  id: integer("id").primaryKey(),
+  id: serial("id").primaryKey(),
   filename: text("filename").notNull(),
-  filepath: text("filepath").notNull(),
+  inviteId: integer("invite_id").references(() => invites.id).notNull(),
   uploadedAt: timestamp("uploaded_at", { mode: "string" }).defaultNow().notNull(),
 });
