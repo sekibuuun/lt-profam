50db1636ddfd339402f2c8fb89dbcac4be8b5edf
sekibuuun <akaunto.no.001@gmail.com>
2025-02-24 17:55:26 +0900

    fix: TypeError: client is not a functionを防ぐため

---
+++ app/[inviteCode]/page.tsx
@@ -0,0 +1,12 @@
+import { notFound } from "next/navigation";
+import { getInviteId, isValidInvite, getInvite } from "@/lib/db";
+import ClientInvitePage from "@/components/ClientInvitePage";
+
+export default async function InvitePage({ params }: { params: { inviteCode: string } }) {
+  if (!(await isValidInvite(params.inviteCode))) {
+    notFound();
+  }
+  const id = await getInviteId(params.inviteCode);
+  const invite = await getInvite(id);
+  return <ClientInvitePage initialFiles={invite.files}/>;
+}
