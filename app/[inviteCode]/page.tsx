366a78a022110a97b13bba841fac311f8e77f767
sekibuuun <akaunto.no.001@gmail.com>
2025-03-07 17:54:38 +0900

    fix: propsを削除

--- app/[inviteCode]/page.tsx
+++ app/[inviteCode]/page.tsx
@@ -1,11 +1,10 @@
 import { notFound } from "next/navigation";
-import { isValidInvite, getFiles } from "@/lib/db";
+import { isValidInvite } from "@/lib/db";
 import ClientInvitePage from "@/components/ClientInvitePage";
 
 export default async function InvitePage({ params }: { params: { inviteCode: string } }) {
   if (!(await isValidInvite(params.inviteCode))) {
     notFound();
   }
-  const files = await getFiles(params.inviteCode);
-  return <ClientInvitePage initialFiles={files}/>;
+  return <ClientInvitePage/>;
 }
